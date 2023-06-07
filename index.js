7d9b200c8f8767995e463e2b20213e5e9824c329
rafaelamicaela <rafaela.micaela@academico.ifrn.edu.br>
2023-06-06 22:50:46 -0300

    update index.js and index.html

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const { app, BrowserWindow } = require('electron')
 
-let win
-app.on('ready', () => {
-    win = new BrowserWindow({
-        width: 800,
-        height: 600,
-    })
-    win.loadFile('index.html')
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600
+  })
+
+  win.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+  createWindow()
 })
